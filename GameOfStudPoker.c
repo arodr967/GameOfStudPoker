e4b25914f673477d585c6f0fc69d9a098b1d994e
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-11-10 18:59:28 -0500

    Create header and main files.

---
+++ GameOfStudPoker.c
@@ -0,0 +1,9 @@
+//
+//  GameOfStudPoker.c
+//  
+//
+//  Created by Alicia Rodriguez on 11/10/15.
+//
+//
+
+#include "GameOfStudPoker.h"
---
+++ GameOfStudPoker.h
@@ -0,0 +1,14 @@
+//
+//  GameOfStudPoker.h
+//  
+//
+//  Created by Alicia Rodriguez on 11/10/15.
+//
+//
+
+#ifndef GameOfStudPoker_h
+#define GameOfStudPoker_h
+
+#include <stdio.h>
+
+#endif /* GameOfStudPoker_h */
