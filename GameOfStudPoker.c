a25c2c9697de4b31be831571ba61e0d62fb2a3e9
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-11-10 19:21:08 -0500

    Edit

--- GameOfStudPoker.c
+++ GameOfStudPoker.c
@@ -1,9 +1,6 @@
 //
 //  GameOfStudPoker.c
 //  
-//
-//  Created by Alicia Rodriguez on 11/10/15.
-//
-//
+
 
 #include "GameOfStudPoker.h"
--- GameOfStudPoker.h
+++ GameOfStudPoker.h
@@ -1,10 +1,7 @@
 //
 //  GameOfStudPoker.h
 //  
-//
-//  Created by Alicia Rodriguez on 11/10/15.
-//
-//
+
 
 #ifndef GameOfStudPoker_h
 #define GameOfStudPoker_h
