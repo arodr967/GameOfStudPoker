86f346b87cfb0d77e33b90c74e72eb0d42a3337f
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2015-11-25 11:41:40 -0500

    Added the ranks
    Added the ranks. Going from 1 (worst) to 10 (best).

--- suit.h
+++ suit.h
@@ -66,6 +66,9 @@ struct players{
 /***********************************
  Prototypes.
  ***********************************/ 
+
+enum ranks {NONE= 0, HIGH_GUARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, STRAIGHT, FLUSH, FULL_HOUSE, FOUR_OF_A_KIND, STRAIGHT_FLUSH, ROYAL_FLUSH};
+ 
 int validateInput(int numberOfCards, int numberOfPlayers);
 char getValue(int type);
 void printDeck(struct deck *newDeck);
